bc3ffd90df06b7da23f7aa4c4be6b9a8db8c6d71
Даниил Глушков <mersik1333@bk.ru>
2024-10-10 13:40:37 +0300

    Добавьте файлы проекта.

---
+++ Task_1.py
@@ -0,0 +1,13 @@
+from re import split
+
+
+print("Введите длины сторон a, b, c треугольнка")   
+print("Сторона a: ") 
+a = float(input())
+print("Сторона b: ") 
+b = float(input())
+print("Сторона c: ") 
+c = float(input())
+
+print("Площадь треугольника равена:", a * b / 2) 
+print("Периметр треугольника равен:", a + b + c) 
